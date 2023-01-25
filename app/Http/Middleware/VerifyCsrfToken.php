18c56960b8079660a3221861bc7e8a4f6b7728ea
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-01-25 18:03:54 +0100

    ddd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
       
-            'payment/*',  // exclude all URLs wit payment/ prefix
+            'payment/*',  
             'user/add', 
             'edit',
             'authors/*', 
